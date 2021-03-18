e83e81cff1fc1622b0ad4052dc99f64228f7777d
Jolie Bonner <2472112b@student.gla.ac.uk>
2021-03-18 01:08:45 +0000

    iniital views commit

--- forum/urls.py
+++ forum/urls.py
@@ -2,17 +2,14 @@ from django.urls import path
 from forum import views
 app_name = 'forum'
 urlpatterns = [
-path('', views.home, name='home'),]
-
-
-'''
-path('create_account/' views.create_account, name = 'create_account'),
-path('sign_in/' views.sign_in, name = 'sign_in'),
-path('about' views.about, name = 'about'),
+path('', views.home, name='home'),
+path('about/', views.about, name = 'about'),
+path('create_account/' ,views.create_account, name = 'create_account'),
+path('sign_in/' ,views.sign_in, name = 'sign_in'),
 path('all_categories/', views.all_categories, name = 'all_categories'),
 path('sign_in/<slug:user_id_slug>/',views.account_info, name='account_info'),
 path('all_categories/create_category', views.create_category, name = 'create_category'),
-path('all_categories/<slug:category_id_slug>/<slug:hack_ID_slug>/' views.Hack name = 'hack'),
-path('all_categories/<slug:category_id_slug>/add_hack/' views.Hack name = 'add_hack'),
-path('all_categories/<slug:category_id_slug>/' views.Category name = 'Category'),
-'''
+path('all_categories/<slug:category_id_slug>/<slug:hack_ID_slug>/', views.hack , name = 'hack'),
+path('all_categories/<slug:category_id_slug>/add_hack/' ,views.add_hack, name = 'add_hack'),
+path('all_categories/<slug:category_id_slug>/', views.category, name = 'category'),
+]
--- forum/views.py
+++ forum/views.py
@@ -1,13 +1,64 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.urls import reverse
+from forum.models import UserAccount, Category, Hack, Comment
 
 def home(request):
-	response = render(request, 'rango/home.html', context=context_dict)
+	#for top 3 of week - returns list of length 3 in order
+	#hackList = Hack.objects.order_by.['-likes'][:3]
+
+
+	context_dict = {}
+	#context_dict['hacks'] = hackList
+	response = render(request, 'forum/home.html', context=context_dict)
 	return response
+	
+	#return HttpResponse("Rango says hey there partner")
 	
 def about(request):
 	context_dict = {}
 	return render(request, 'forum/about.html', context=context_dict)
+
+	
+def create_category(request):
+	context_dict = {}
+	response = render(request, 'forum/create_category.html', context=context_dict)
+	return response
+	
+def category(request):
+	#categoryList = Category.object
+	context_dict = {}
+	response = render(request, 'forum/category.html', context=context_dict)
+	return response
+
+def hack(request):
+	context_dict = {}
+	response = render(request, 'forum/hack.html', context=context_dict)
+	return response
+	
+def add_hack(request):
+	context_dict = {}
+	response = render(request, 'forum/add_hack.html', context=context_dict)
+	return response
+
+def all_categories(request):
+	context_dict = {}
+	response = render(request, 'forum/home.html', context=context_dict)
+	return response
+
+def account_info(request):
+	context_dict = {}
+	response = render(request, 'forum/account_info.html', context=context_dict)
+	return response
+	
+def create_account(request):
+	context_dict = {}
+	response = render(request, 'forum/create_account.html', context=context_dict)
+	return response
+
+def sign_in(request):
+	context_dict = {}
+	response = render(request, 'forum/sign_in.html', context=context_dict)
+	return response
 
 
