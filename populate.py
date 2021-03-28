f497e1582b6e0f00d8f933d0d2318922aefd2322
davidfr1999 <64556566+davidfr1999@users.noreply.github.com>
2021-03-28 12:53:56 +0100

    Update populate.py

--- populate.py
+++ populate.py
@@ -13,83 +13,83 @@ from pytz import utc
 
 
 def populate():
-    user_accounts = [{'username': 'adamr55',
-                      'password': 'chick3ns',
-                      'email': 'adam1233@gmail.com',
+    user_accounts = [{'user': 'adamr55',
                       'verified': True},
                      {'username': 'sarahp3',
-                      'password': 'glasgow321',
-                      'email': 'sarahp3@hotmail.com',
                       'verified': False},
                      {'username': 'lifehackfan',
-                      'password': 'Wx4tt6a1',
-                      'email': 'nicola22@gmail.com',
                       'verified': False},
                      {'username': 'jasper999',
-                      'password': 'kitten11',
-                      'email': 'jasper999@live.co.uk',
                       'verified': True},
                      {'username': 'davidf99',
-                      'password': 'london128',
-                      'email': 'davidf@outlook.com',
                       'verified': True}]
 
     categories = [{'categoryName': 'Cleaning',
-                   'userName': 'adamr55',
-                   'description': 'Hacks related to cleaning'},
+                   'user': 'adamr55',
+                   'description': 'Hacks related to cleaning',
+                    'slug': 'cleaning'},
                   {'categoryName': 'Beauty and Fashion',
-                   'userName': 'jasper999',
-                   'description': 'Hacks related to beauty and fashion'},
+                   'user': 'jasper999',
+                   'description': 'Hacks related to beauty and fashion',
+                    'slug': 'beauty-and-fashion'},
                   {'categoryName': 'Parenting',
-                   'userName': 'sarahp3',
-                   'description': 'Hacks related to being a parent'},
+                   'user': 'sarahp3',
+                   'description': 'Hacks related to being a parent',
+                    'slug': 'parenting'},
                   {'categoryName': 'Organising',
-                   'userName': 'davidf99',
-                   'description': 'Hacks related to organising'},
+                   'user': 'davidf99',
+                   'description': 'Hacks related to organising',
+                    'slug': 'organising'},
                   {'categoryName': 'Cooking',
-                   'userName': 'lifehackfan',
-                   'description': 'Hacks related to food and cooking.'}]
+                   'user': 'lifehackfan',
+                   'description': 'Hacks related to food and cooking.',
+                    'slug': 'cooking'}]
 
     hacks = [{'hackID': 1,
               'name': 'Fresh Bananas',
               'description': 'By covering the stems of your bananas with plastic wrap you can keep them fresher for longer',
               'shortDescription': 'Put plastic wrap over banana stems to keep them fresh',
               'likes': 35,
-              'userName': 'lifehackfan',
+              'user': 'lifehackfan',
               'categoryName': 'Cooking',
-              'dateTimeCreated': None},
+              'dateTimeCreated': None,
+              'slug': 'fresh-bananas'},
              {'hackID': 2,
               'name': 'Zip Hack',
               'description': 'Is your jeans zipper falling down? Use a keyring loop to fasten the zip to a button and keep them up!',
               'shortDescription': 'Use a metal loop to keep jeans from falling down',
               'likes': 12,
-              'userName': 'jasper999',
+              'user': 'jasper999',
               'categoryName': 'Fashion',
-              'dateTimeCreated': None},
+              'dateTimeCreated': None,
+              'slug': 'zip-hack'},
              {'hackID': 3,
               'name': 'Mosquito Net Hack',
               'description': "Put a bedsheet over your child's outdoor play area to prevent bugs from biting them",
               'shortDescription': 'Use a sheet to protect your child from bugs',
               'likes': 44,
-              'userName': 'sarahp3',
+              'user': 'sarahp3',
               'categoryName': 'Parenting',
-              'dateTimeCreated': None},
+              'dateTimeCreated': None,
+              'slug': 'mosquito-net-hack'},
              {'hackID': 4,
               'name': 'Bag Storage',
               'description': 'Store any plastic bags you have in an empty packet of wipes for easy storage and retrieval',
               'shortDescription': 'Use empty wipe packets to store carrier bags',
               'likes': 30,
-              'userName': 'davidf99',
+              'user': 'davidf99',
               'categoryName': 'Organising',
-              'dateTimeCreated': None},
+              'dateTimeCreated': None,
+              'slug': 'bag-storage'},
              {'hackID': 5,
               'name': 'Foam Plate Hack',
               'description': 'Put foam plates in between dishes to avoid them getting chipped when storing them',
               'shortDescription': 'Use foam plates to protect dishes',
               'likes': 8,
-              'userName': 'lifehackfan',
+              'user': 'lifehackfan',
               'categoryName': 'Cooking',
-              'dateTimeCreated': None}
+              'dateTimeCreated': None,
+              'slug': 'foam-plate-hack'}
              ]
 
     comments = [{'commentID': 1,
@@ -120,58 +120,58 @@ def populate():
                 ]
 
     for user_account in user_accounts:
-        AddUserAccount(user_account['username'], user_account['password'],
-                           user_account['email'], user_account['verified'])
+        AddUserAccount(user_account['user'], user_account['verified'])
 
     for category in categories:
-        AddCategory(category['categoryName'], category['username'], category['description'])
+        AddCategory(category['categoryName'], category['user'], category['description'], category['slug'])
 
     for hack in hacks:
         AddHack(hack['hackID'], hack['name'], hack['description'], hack['shortDescription'], hack['likes'],
-                hack['userName'], hack['categoryName'], hack['dateTimeCreated'])
+                hack['user'], hack['categoryName'], hack['dateTimeCreated'], hack['slug'])
 
     for comment in comments:
-        AddComment(comment['commentID'], comment['hackID'], hack['userName'], hack['text'], hack['dateTimeCreated'])
+        AddComment(comment['commentID'], comment['hackID'], hack['user'], hack['text'], hack['dateTimeCreated'])
 
 
 
 
-def AddUserAccount(username, password, email, verified):
-    u = UserAccount.objects.get_or_create(userName=username, password=password, email=email, verified=verified)[0]
-    u.userName = username
-    u.password = password
-    u.email = email
+def AddUserAccount(user, verified):
+    u = UserAccount.objects.get_or_create(user=user, verified=verified)[0]
+    u.user = user
     u.verified = verified
     u.save()
     return u
 
 
-def AddCategory(categoryName, userName, description):
-    cat = Category.objects.get_or_create(categoryName=categoryName, userName=userName, description=description)[0]
+def AddCategory(categoryName, user, description, slug):
+    cat = Category.objects.get_or_create(categoryName=categoryName, user=user, description=description, slug=slug)[0]
     cat.categoryName = categoryName
-    cat.userName = userName
+    cat.user = user
     cat.description = description
+    cat.slug = slug
     cat.save()
     return cat
 
 def AddHack(hackID, name, description, shortDescription, likes, userName, categoryName, dateTimeCreated):
     h = Hack.objects.get_or_create(hackID=hackID, name=name, description=description, shortDescription=shortDescription,
-                                   likes=likes, userName=userName, categoryName=categoryName, dateTimeCreated=dateTimeCreated)[0]
+                                   likes=likes, user=user, categoryName=categoryName, dateTimeCreated=dateTimeCreated, slug=slug)[0]
     h.hackID = hackID
     h.name = name
     h.description = description
     h.shortDescription=shortDescription
     h.likes=likes
-    h.userName=userName
+    h.user=user
     h.categoryName=categoryName
     h.dateTimeCreated=dateTimeCreated
+    h.slug=slug
     h.save()
     return h
 
 def AddComment(commentID, hackID, userName, text, dateTimeCreated):
-    com = Comment.objects.get_or_create(commentID=commentID, hackID=hackID, text=text, dateTimeCreated=dateTimeCreated)[0]
+    com = Comment.objects.get_or_create(commentID=commentID, hackID=hackID,, user=user text=text, dateTimeCreated=dateTimeCreated)[0]
     com.commentID = commentID
     com.hackID = hackID
+    com.user = user
     com.text = text
     com.dateTimeCreated=dateTimeCreated
     com.save()
