2400b984c69f4598d3065881d6523cc207cdbb85
Frederik G <glitznerf@gmail.com>
2021-04-03 14:19:36 +0100

    JS Comment form validation

--- static/javascript/hack.js
+++ static/javascript/hack.js
@@ -1,9 +1,22 @@
 window.onload = function () {
-  let star = document.getElementById("star")
-  star.onclick = function() {myFunction()};
+  let star = document.getElementById("star");
+  star.addEventListener("click", starTransform);
 
-  function myFunction() {
-    star.style.backgroundColor="yellow";
-    star.style.width="500px";
+  let commentCheck = document.getElementById("checkComment");
+  commentCheck.addEventListener("click", checkComment);
+}
+
+function starTransform() {
+  star.style.backgroundColor="yellow";
+  star.style.width="500px";
+}
+
+function checkComment() {
+  let comment = document.getElementById("new_comment");
+  entryValue = comment.value;
+
+  if(entryValue.length>0) {
+    let submit = document.getElementById("submitButton");
+    submit.click();
   }
 }
