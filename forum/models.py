2e4e7255449bd4a5c18d2c8b994499aaa6a22098
Angus Wilson <2466402W@student.gla.ac.uk>
2021-04-03 17:11:21 +0100

    modified the database to auto cascade dellete and also added comments to models.py

--- forum/models.py
+++ forum/models.py
@@ -2,31 +2,25 @@ from django.db import models
 from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
 
-"""
-made all primary keys explicet for clarity
-
-UserAccount implement the django user model
-
-category, hack and comment all store the user that created them rather than 
-just the username, this will have to be aquired in either forms.py or views.py
-
-need to add an optional image field to Hack at some point but need to
- mess about with the media root etc to do that 
 
 """
-
-
-def getNoUsernameText():
-    return "!!!No Username!!!"
-
-
+UserAccount model has 2 fields: 
+user which implements the django user interface and is pimary key and cascades on dellete
+verified which is a boolean value used to keep track whether a suer is verified and has acces to certain features of the webapp
+"""
 class UserAccount(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,unique=True, primary_key=True)
     verified = models.BooleanField(default=False)
     def __str__(self):
         return self.user.username
 
-
+"""
+Category model is used to store information on all of the categories, it has 4 fields:
+categoryName is the name of the category and is primary key
+user is a foriegn key referancing the user that created the category, if this user is deleted this field is set to null
+description is a descrition of the content of the category
+slug is the slugified category name used in URLS and is updated autimatically every time the model is saved
+"""
 class Category(models.Model):
     categoryName = models.CharField(max_length=20, unique=True, primary_key=True)
     user = models.ForeignKey(UserAccount, null=True, on_delete=models.SET_NULL)
@@ -40,14 +34,26 @@ class Category(models.Model):
     def __str__(self):
         return self.slug
 
-
+"""
+Hack is used to store information on each of the hack posts on the site, it has 10 fields:
+hackID is an auto incremented primary key field 
+name is the name of the hack 
+description is a description of the hack
+shortDescription is used for when displaying the hacks on the category page so that there isnt too much text on the page
+likes is an integer to keep track of the number of likes the page has, this can then be used for quires to find the most popular hacks 
+user is a foriegn key referancing the user that created the hack, if this user is deleted the hack is also delleted 
+categoryName is a foriegn key referancing the category that the hack belongs in, if this category is deleted the hack is also delleted
+dateTimeCreated is a dateTime field that is initialised to the time the hack is created, this can then be used in queries to get the most recent created hack
+slug is the slugified name used in URLS and is updated autimatically every time the model is saved
+image is a imageField that makes use of pillow, the image is set to a default image and the images are stored in hackImages folder inside the media root folder
+"""
 class Hack(models.Model):
     hackID = models.AutoField(primary_key=True)
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=500)
     shortDescription = models.CharField(max_length=55)
     likes = models.IntegerField(default=0)
-    user = models.ForeignKey(UserAccount, null=True, on_delete=models.SET_NULL)
+    user = models.ForeignKey(UserAccount, null=True, on_delete=models.CASCADE)
     categoryName = models.ForeignKey(Category, on_delete=models.CASCADE)
     dateTimeCreated = models.DateTimeField(auto_now_add = True)
     slug = models.SlugField(blank=True)
@@ -58,11 +64,18 @@ class Hack(models.Model):
     def __str__(self):
         return str(self.hackID)
 
-
+"""
+Comment is used to store all of the data on each of the comments made by user on a given hack, it has 5 fields:
+commentID is an auto incremented primary key field 
+hackID is a foriegn key referancing the hack that the comment is on, if this hack is deleted the comment is also delleted
+user is a foriegn key referancing the user that created the comment, if this user is deleted the comment is also delleted 
+text is the main text content of the comment 
+dateTimeCreated is a dateTime field that is initialised to the time the comment is created
+"""
 class Comment(models.Model):
     commentID = models.AutoField(primary_key=True)
     hackID = models.ForeignKey(Hack, on_delete=models.CASCADE)
-    user = models.ForeignKey(UserAccount, null=True, on_delete=models.SET_NULL)
+    user = models.ForeignKey(UserAccount, null=True, on_delete=models.CASCADE)
     text = models.CharField(max_length=255)
     dateTimeCreated = models.DateTimeField(auto_now_add = True)
 
